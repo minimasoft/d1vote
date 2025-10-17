db72313b75c42fe84d70ce81150151e850fdd24e
Minimasoft <minimasoft@proton.me>
2025-10-17 22:48:12 +0000

    fix

--- migrations/0002_make_votekey_unique.sql
+++ migrations/0002_make_votekey_unique.sql
@@ -1,12 +1,3 @@
-BEGIN TRANSACTION;
-
--- Remove duplicate rows, keeping the first row for each votekey (by rowid)
-DELETE FROM votekeys
-WHERE rowid NOT IN (
-  SELECT MIN(rowid) FROM votekeys GROUP BY votekey
-);
 
 -- Enforce uniqueness on the votekey column
 CREATE UNIQUE INDEX IF NOT EXISTS idx_votekeys_votekey_unique ON votekeys(votekey);
-
-COMMIT;
